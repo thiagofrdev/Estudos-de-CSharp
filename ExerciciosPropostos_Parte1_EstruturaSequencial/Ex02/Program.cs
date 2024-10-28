cd5ae7b6abcbf0c4fcb3e97b9181aa0020f18865
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-10-28 18:36:06 -0300

    Exercício 02 (Estrutura Sequencial)

---
+++ ExerciciosPropostos_Parte1_EstruturaSequencial/Ex02/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Faça um programa para ler o valor do raio de um círculo, e depois mostrar o valor da área deste círculo com quatro casas decimais conforme exemplos.
+Fórmula da área: area = π . raio2
+Considere o valor de π = 3.14159
+*/
+Console.Write("Valor do raio: ");
+double raio = int.Parse(Console.ReadLine());
+double pi = 3.14159;
+
+Console.Write($"A aérea do círculo é {pi*Math.Pow(raio, 2):F2}");

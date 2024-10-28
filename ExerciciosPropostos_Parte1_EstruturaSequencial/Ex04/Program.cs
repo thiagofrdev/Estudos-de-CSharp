4841e0cae0eaec7e85f6d0b96aabfb6c2d9042b9
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-10-28 20:14:53 -0300

    Exercício 04 (Estrutura Sequencial)

---
+++ ExerciciosPropostos_Parte1_EstruturaSequencial/Ex04/Program.cs
@@ -0,0 +1,11 @@
+﻿// Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas decimais.
+using System.Globalization;
+
+Console.Write("Número: ");
+int numero = int.Parse(Console.ReadLine());
+Console.Write("Hrs. trabalhadas: ");
+int horasTrabalhadas = int.Parse(Console.ReadLine());
+Console.Write("Val. por hora: ");
+double valorHora = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+Console.Write($"NUMBER = {numero}\nSALARY = U${horasTrabalhadas*valorHora:F2}");

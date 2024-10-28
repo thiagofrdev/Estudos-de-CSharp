ce986f2acffce4405825d3f681f1c9da4766c2ac
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-10-28 18:29:28 -0300

    Commit inicial. Primeiro exercicio (Estrutura Sequencial)

---
+++ ExerciciosPropostos_Parte1_EstruturaSequencial/Ex01/Program.cs
@@ -0,0 +1,7 @@
+﻿//Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma mensagem explicativa, conforme exemplos.
+Console.Write("Valor 1: ");
+int valor1 = int.Parse(Console.ReadLine());
+Console.Write("Valor 2: ");
+int valor2 = int.Parse(Console.ReadLine());
+
+Console.Write($"SOMA = {valor1+valor2}");

e208cb7a11dc5b831c4eb8a14798e72baddcefad
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-10-28 19:18:10 -0300

    Exercício 3 (Estrutura Sequencial)

---
+++ ExerciciosPropostos_Parte1_EstruturaSequencial/Ex03/Program.cs
@@ -0,0 +1,11 @@
+﻿//Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).
+Console.Write("Valor A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Valor B: ");
+int B = int.Parse(Console.ReadLine());
+Console.Write("Valor C: ");
+int C = int.Parse(Console.ReadLine());
+Console.Write("Valor D: ");
+int D = int.Parse(Console.ReadLine());
+
+Console.Write($"DIFERENCA = {A*B - C*D}");

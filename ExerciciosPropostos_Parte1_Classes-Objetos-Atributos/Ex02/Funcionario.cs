44f4cbc4f9374eae5d78607fa9b9003112d940a1
Thiago Ferreira Ribeiro <thiago26ribeiro@gmail.com>
2024-10-28 21:57:55 -0300

    Exercício 02 (Classes, Objetos e Atributos)

---
+++ ExerciciosPropostos_Parte1_Classes-Objetos-Atributos/Ex02/Funcionario.cs
@@ -0,0 +1,8 @@
+class Funcionario{
+    public string nome;
+    public double salario;
+
+    public static double mediaSalario(double salario1, double salario2){
+        return (salario1+salario2)/2;
+    }
+}
---
+++ ExerciciosPropostos_Parte1_Classes-Objetos-Atributos/Ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Fazer um programa para ler nome e salário de dois funcionários. Depois, mostrar o salário médio dos funcionários.
+Funcionario fA, fB;
+
+fA = new Funcionario(); 
+fB = new Funcionario();
+
+Console.WriteLine("Dados do primeiro funcionário:");
+Console.Write("Nome: ");
+fA.nome = Console.ReadLine();
+Console.Write("Salário: ");
+fA.salario = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Dados do segundo funcionário:");
+Console.Write("Nome: ");
+fB.nome = Console.ReadLine();
+Console.Write("Salário: ");
+fB.salario = double.Parse(Console.ReadLine());
+
+Console.Write("Salário médico = R$" + Funcionario.mediaSalario(fA.salario, fB.salario));
